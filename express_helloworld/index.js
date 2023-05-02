112655da625427384e3d91c54bdb4dd3a2532546
MarcoSpinaBZZ <spinam@bzz.ch>
2023-05-02 14:53:38 +0000

    add index.js with Hello World

---
+++ express_helloworld/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/now", (req, res) => {
+    res.send(new Date().toISOString());
+});
+
+app.listen(port, () => {
+    console.log(`Server ist gestartet :`);
+});
