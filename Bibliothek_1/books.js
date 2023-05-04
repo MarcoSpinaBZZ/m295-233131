bea0a1ffc456037da9155d72e4453b3261fa54aa
MarcoSpinaBZZ <spinam@bzz.ch>
2023-05-04 12:48:39 +0000

    add lend functions vom Bibliothek 1 Auftrag

--- Bibliothek_1/books.js
+++ Bibliothek_1/books.js
@@ -37,6 +37,52 @@ let books = [
     },
 ]
 
+let loans = [
+    {
+        id: "1",
+        customer_id: "1",
+        isbn: "10-1758-9937-1",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-01T00:00:00.000Z"
+    },
+    {
+        id: "2",
+        customer_id: "1",
+        isbn: "0-8347-8520-7",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-05T00:00:00.000Z"
+    },
+    {
+        id: "3",
+        customer_id: "1",
+        isbn: "0-6063-5481-6",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-11T00:00:00.000Z"
+    },
+    {
+        id: "4",
+        customer_id: "2",
+        isbn: "0-5345-9930-3",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-12T00:00:00.000Z"
+    },
+    {
+        id: "5",
+        customer_id: "2",
+        isbn: "0-1722-6813-3",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-18T00:00:00.000Z"
+    },
+    {
+        id: "6",
+        customer_id: "2",
+        isbn: "0-1210-9922-9",
+        borrowed_at: new Date().toISOString(),
+        returned_at: "2023-07-20T00:00:00.000Z"
+    },
+]
+
+
 export function findAll() {
     return books;
 }
@@ -62,4 +108,22 @@ export function replace(book) {
 
 export function remove(isbn) {
     books = books.filter((b) => b.isbn !== isbn)
+}
+
+
+
+export function findAllLoans() {
+    return loans;
+}
+
+export function findByLoan(id) {
+    return loans.find((l) => l.id == id);
+}
+
+export function borrowNewBook(loan) {
+    loans = [...loans, loan];
+}
+
+export function changeDateOfLoan(loan) {
+    loans = loans.map((l) => l.id == loan.id ? loan : l);
 }
