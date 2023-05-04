7598ceaa47c99c5b2456e21a2eae86c85e9c7cce
MarcoSpinaBZZ <spinam@bzz.ch>
2023-05-04 12:50:25 +0000

    Add Bibliothek1 Aufgabe Ressource Lend ohne zusatz

--- Bibliothek_1/bibliothek.js
+++ Bibliothek_1/bibliothek.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { findAll, findByISBN, insert, replace, remove } from "./books.js";
+import { findAllLoans, findByLoan, borrowNewBook, changeDateOfLoan } from "./books.js";
 
 const app = express();
 const port = 3001;
@@ -55,6 +56,46 @@ app.delete("/books/:isbn", (req, res) => {
     remove(req.params.isbn);
     res.sendStatus(204);
 })
+
+
+
+// show all loans
+app.get("/lends", (req, res) => {
+    res.send(findAllLoans());
+});
+
+// show one loan
+app.get("/lends/:id", (req, res) => {
+    res.send(findByLoan(req.params.id));
+});
+
+// lend new book
+app.post("/lends", (req, res) => {
+    const newLoan = [
+        {
+            id: "7",
+            customer_id: "2",
+            isbn: "0-1562-1863-8",
+            borrowed_at: new Date().toISOString(),
+            returned_at: "2023-07-23T00:00:00.000Z"
+        }
+    ];
+    borrowNewBook(newLoan);
+    res.sendStatus(201);
+});
+
+// change date of loan
+app.patch("/lends/:id", (req, res) => {
+    const changedLoan ={
+            id: req.params.id,
+            customer_id: "1",
+            isbn: "10-1758-9937-1",
+            borrowed_at: new Date().toISOString(),
+            returned_at: ""
+        };
+    changeDateOfLoan(changedLoan);
+    res.sendStatus(204);
+});
 
 app.listen(port, () => {
     console.log(`Server ist gestartet :)`);
