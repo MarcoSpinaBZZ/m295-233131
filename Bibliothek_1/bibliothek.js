7b814b1c6957a8fc9162e20592d77dc0d6e51ad8
MarcoSpinaBZZ <spinam@bzz.ch>
2023-05-04 09:39:50 +0000

    add Bibliothek 1 Aufträge alle gemacht

--- Bibliothek_1/bibliothek.js
+++ Bibliothek_1/bibliothek.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import multer from "multer";
+import { findAll, findByISBN, insert, replace, remove } from "./books.js";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
@@ -13,6 +13,48 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+// show all books
+app.get("/books", (req, res) => {
+    res.send(findAll());
+});
+
+// show one book
+app.get("/books/:isbn", (req, res) => {
+    res.send(findByISBN(req.params.isbn));
+});
+
+// create book
+app.post("/books", (req, res) => {
+    const newBook = [
+        {
+            isbn: "7",
+            title: "Japanese book",
+            year: 2021,
+            author: "Mr. Nippon"
+        }
+    ];
+    insert(newBook);
+    res.sendStatus(201);
+});
+
+// replace book
+app.put("/books/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    const replacedBook ={
+            isbn: isbn,
+            title: "Ruby book",
+            year: 1995,
+            author: "Mr. Diamond"
+        };
+    replace(replacedBook);
+    res.sendStatus(204);
+});
+
+// delete book
+app.delete("/books/:isbn", (req, res) => {
+    remove(req.params.isbn);
+    res.sendStatus(204);
+})
 
 app.listen(port, () => {
     console.log(`Server ist gestartet :)`);
